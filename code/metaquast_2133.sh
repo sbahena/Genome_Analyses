5080beb504c1fa08f0d9401d9f29af225e56981c
sbahena <47498893+sbahena@users.noreply.github.com>
2019-05-20 17:46:51 +0200

    Update metaquast_2133.sh

--- code/metaquast_2133.sh
+++ code/metaquast_2133.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -l
+
 #SBATCH -A g2019003
 #SBATCH -p core
 #SBATCH -n 2
@@ -6,8 +7,10 @@
 #SBATCH -J evaluation_SB_4_Trash
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user silvia.bahena13@hotmail.com
-# Load modules
+
+# load modules
 module load bioinfo-tools
 module load quast/4.5.4
+
 # Your commands
-python /sw/apps/bioinfo/quast/4.5.4/rackham/bin/metaquast.py  -o ~/results/assembly/evaluation_33 ~/results/assembly/assembly_DNA_2133_final/final.contigs.fa -t 2 -1 ~/raw_data/DNA_trimmed/SRR4342133_1.paired.trimmed.fastq.gz -2 ~/raw_data/DNA_trimmed/SRR4342133_2.paired.trimmed.fastq.gz
+metaquast.py ~/results/assembly/assembly_DNA_2133_final/final.contigs.fa -o ~/results/assembly/evaluation_33 -t 2 -1 ~/raw_data/DNA_trimmed/SRR4342133_1.paired.trimmed.fastq.gz -2 ~/raw_data/DNA_trimmed/SRR4342133_2.paired.trimmed.fastq.gz
