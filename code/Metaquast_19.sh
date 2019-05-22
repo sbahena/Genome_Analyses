534ea67d439e82c856b7c3729c62a77e08379875
sbahena <47498893+sbahena@users.noreply.github.com>
2019-05-22 14:23:34 +0200

    Add files via upload

---
+++ code/Metaquast_19.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+
+#SBATCH -A g2019003
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 02:00:00
+#SBATCH -J evaluation_SB_4_Trash
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user silvia.bahena13@hotmail.com
+
+# load modules
+module load bioinfo-tools
+module load quast/4.5.4
+
+# Your commands
+metaquast.py  ~/results/assembly/assembly_DNA_2119/final.contigs.fa -o ~/results/assembly/evaluation_19 -t 2 -1 ~/raw_data/DNA_trimmed/SRR4342129_1.paired.trimmed.fastq.gz -2 ~/raw_data/DNA_trimmed/SRR4342129_2.paired.trimmed.fastq.gz
