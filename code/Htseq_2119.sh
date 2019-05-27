b1af5a7bef6b3012408771433414019207d38ad1
sbahena <47498893+sbahena@users.noreply.github.com>
2019-05-27 11:59:48 +0200

    Htseq 2119

--- code/Htseq_2119.sh
+++ code/Htseq_2119.sh
@@ -18,12 +18,12 @@ module load htseq
 dir=/domus/h1/silvi/results/map/map_2119
 
 
-for i in 4 8 9 10
+for i in {1..12}
 do
 	htseq-count -f bam  -r pos -t  CDS  -i  ID --stranded=no ${dir}/map_2119_$i$'P'.sorted.bam <(sed '/##FASTA/Q' /domus/h1/silvi/results/prokka/prokka_2119/bin_2119_$i/prokka_2119_$i.gff) > /domus/h1/silvi/results/htseq/htseq_2119/htseq2119_$i$'P'.out
 done
 
-for i in 4 8 9 10
+for i in  {1..12}
 do
         htseq-count -f bam  -r pos -t  CDS  -i  ID --stranded=no ${dir}/map_2119_$i$'U'.sorted.bam <(sed '/##FASTA/Q' /domus/h1/silvi/results/prokka/prokka_2119/bin_2119_$i/prokka_2119_$i.gff) > /domus/h1/silvi/results/htseq/htseq_2119/htseq2119_$i$'U'.out
 done
